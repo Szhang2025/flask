57bdf15569a60f91f239590fc3597d51d8685f2c
Szhang2025 <szhang@stcloudstate.edu>
2026-09-09 18:32:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// =========================================
+// Data Analysis App - JavaScript
+// =========================================
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Data Analysis App loaded.");
+
+});
